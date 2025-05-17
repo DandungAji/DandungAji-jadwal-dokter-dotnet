a9c107b61cca2d18b07b7392b713ff02ef159bf5
DandungAji <dandungajira@gmail.com>
2025-05-17 17:23:11 +0700

    feat: tambah koneksi SQL Server dan model DoctorSchedule

---
+++ Models/DoctorSchedule.cs
@@ -0,0 +1,7 @@
+public class DoctorSchedule
+{
+    public string NamaDokter { get; set; }
+    public string Spesialis { get; set; }
+    public string Hari { get; set; }
+    public string Jam { get; set; }
+}
